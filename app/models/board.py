cdc85ec519abd64d3a9f289c3683ff63663f8476
Amanda <thompson.adg@gmail.com>
2025-12-23 13:47:02 -0800

    Created board and card_board_routes (#1)
    * Implement board and card creation routes with validation and error handling
    
    * Refactor routes and utilities: enable board and card routes, implement model creation and validation functions

--- app/models/board.py
+++ app/models/board.py
@@ -3,17 +3,23 @@ from ..db import db
 
 class Board(db.Model):
     board_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    title: Mapped[str]
-    owner: Mapped[str]
+    title: Mapped[str] = mapped_column(nullable=False)
+    owner: Mapped[str] = mapped_column(nullable=False)
     cards: Mapped[list["Card"]] = relationship("Card", back_populates="board")
 
     def to_dict(self):
-        return {
+        board_dict = {
             "board_id": self.board_id,
             "title": self.title,
-            "owner": self.owner
+            "owner": self.owner,
+            "cards": [card.to_dict() for card in self.cards]
         }
+
+        return board_dict
 
     @classmethod
     def from_dict(cls, board_data):
-        return cls(title=board_data["title"], owner=board_data["owner"])
+        title = board_data["title"]
+        owner = board_data["owner"]
+
+        return cls(title=title, owner=owner)
---
+++ app/routes/board_routes.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, request, abort, make_response
+from app.models.board import Board
+from app.models.card import Card
+from ..db import db
+from ..routes.routes_utilities import create_model, validate_model
+
+bp = Blueprint("boards_bp", __name__, url_prefix="/boards")
+
+@bp.post("")
+def create_board():
+    request_body = request.get_json()
+    return create_model(Board, request_body)
+
+
+@bp.post("/<board_id>/cards")
+def create_card_on_board(board_id):
+    board = validate_model(Board, board_id)
+    request_body = request.get_json()
+    return create_model(Card, {**request_body, "board_id": board.board_id})
+
