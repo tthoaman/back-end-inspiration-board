d2b5b29f078b531653cb4a6c9406e0408ca946e0
liyan <liyangeneral@gmail.com>
2026-01-02 10:53:10 -0800

    fix routes,edit card in card_routes

--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, Response
+from flask import Blueprint, Response,request
 from app.models.card import Card
 from app.routes.route_utilities import validate_model
 from ..db import db
@@ -25,3 +25,17 @@ def like_card(card_id):
         "message": card.message,
         "likes_count": card.likes_count
     }, 200
+
+@bp.patch("<card_id>")
+def edit_card(card_id):
+    card = validate_model(Card, card_id)
+    data = request.get_json()
+
+    if "message" in data:
+        card.message = data["message"]
+    if "likes_count" in data:
+        card.likes_count = data["likes_count"]
+    
+    db.session.commit()
+    
+    return card.to_dict(), 200
